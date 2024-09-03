df633167dabed043323a2dcdc864f92c6f6121e0
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2024-09-03 20:39:34 +0400

    Basic_cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""Task 0: Basic Dictornary"""
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """A class `BaseCache` that inherits from `BaseCaching`
+    and is a caching system.
+    """
+
+    def put(self, key, item):
+        """
+        assign to the dictionary self.cache_data the
+        item value for the key key
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key
+        """
+
+        return self.cache_data.get(key, None)
+
